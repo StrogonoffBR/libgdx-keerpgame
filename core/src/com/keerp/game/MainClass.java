7704c3c60e95dd0a9cd273aca9b4a03437fae407
Vitor Leal <ikeerpbr@gmail.com>
2016-02-12 23:47:04 -0200

    Changing the background color

--- core/src/com/keerp/game/MainClass.java
+++ core/src/com/keerp/game/MainClass.java
@@ -53,7 +53,7 @@ public class MainClass implements ApplicationListener {
 		}
 				
 		// setting screen color
-		Gdx.gl.glClearColor(24, 243, 255, 1);
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f,0xff/255.0f);
 		
 		// clear the screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
